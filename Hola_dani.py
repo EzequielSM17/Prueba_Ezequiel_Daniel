017a1c40ad96147daa3e164ed853cac3d232d7ea
EzequielSM17 <ezequielsolermartinez@gmail.com>
2021-03-09 19:31:49 +0100

    Funcion feo cambiada

--- Hola_dani.py
+++ Hola_dani.py
@@ -3,12 +3,13 @@ def saluda():
 def Contar(num):
     saluda()
     print("hola dani"*num)
-    print("hola cheke")
+    print('hola "cheke"')
 def feo():
-    print("hola genaro")
+    return "hola genaro"
 
 def hola():
     print("hola"*8)
 hola()
 saluda()
 Contar(2)
+print(feo()*4)
